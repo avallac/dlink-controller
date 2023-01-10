c889af2d5f45fe7818692a5c927bb571488abc00
Petr Petrenko <petr.petrenko@team.bumble.com>
2023-01-11 02:06:24 +0400

    массовое обновление свичей

--- bin/dgs121010mp_checker.php
+++ bin/dgs121010mp_checker.php
@@ -21,10 +21,14 @@ $ip = $argv[1];
 $dev = new \Avallac\DlinkController\DGS121010MP($ip, $config['config'], $argv[2]);
 print "Старт проверки \033[36m$ip\033[0m\n";
 $result = $dev->check($update);
+$errors = 0;
 
 foreach ($result as $item) {
     print "\033[33m" . $item['name'] . "\033[0m - " . ($item['result'] ? 'OK' : "\033[31mKO\033[0m") . "\n";
     foreach ($item['error'] as $line) {
+        $errors++;
         print "-> \033[31m" .  $line[0] . "\033[0m\n";
     }
 }
+
+return exit((int)!empty($errors));
